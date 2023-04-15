a4bed261e275d43bb4ca0546b2252cd4b40bdb6d
baizhi958216 <1475289190@qq.com>
2023-04-16 00:47:59 +0800

    feat: Get, Post控制器
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,9 @@ import {
   Param,
   Delete,
   Version,
+  Query,
+  HttpCode,
+  Headers,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -21,15 +24,47 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  /* create(@Request() req) {
+    console.log(req.body);
+    return {
+      code: 200,
+      message: req.body.name,
+    };
+  } */
+  // 使用 @Body() 装饰器取出req.body
+  /* create(@Body() req: { name: string }) {
+    console.log(req.name);
+    return {
+      code: 200,
+      message: req.name,
+    };
+  } */
+  // 使用 @Body(param) 装饰器取出指定参数值
+  create(@Body('age') age: number) {
+    console.log(age);
+    return {
+      code: 200,
+      message: age,
+    };
   }
 
   @Get()
   // 单独控制, 优先级高于接口全局版本控制
   @Version('v2')
-  findAll() {
-    return this.userService.findAll();
+  /* findAll(@Request() req) {
+    console.log(req);
+    return {
+      code: 200,
+      message: req.query.name,
+    };
+  } */
+  // 使用 @Query() 装饰器取出req.query
+  findAll(@Query() req: { name: string }) {
+    console.log(req);
+    return {
+      code: 200,
+      message: req.name,
+    };
   }
 
   @Get(':id')
@@ -37,6 +72,25 @@ export class UserController {
   @Version(['version1', 'v2', 'v3'])
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
+  }
+
+  @Get(':id')
+  /* findId(@Request() req) {
+    console.log(req.params.id);
+    return {
+      code: 200,
+      message: req.params.id,
+    };
+  } */
+  // 设置返回状态码
+  @HttpCode(500)
+  // 使用 @Param() 装饰器取出req.param
+  findId(@Param() req, @Headers() headers) {
+    console.log(headers);
+    return {
+      code: 200,
+      message: req.id,
+    };
   }
 
   @Patch(':id')
