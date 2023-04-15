f8d518a6919587b2e7f0b42433def4379c409578
baizhi958216 <1475289190@qq.com>
2023-04-16 01:05:17 +0800

    feat: 加入Session
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,20 @@ async function bootstrap() {
     type: VersioningType.URI,
     prefix: false,
   });
+  app.use(
+    session({
+      // 后端session签名(加盐)
+      secret: 'XiaoMan',
+      // 每次请求时强行设置cookies(会重置过期时间)
+      rolling: true,
+      // 返回前端cookie名字
+      name: 'xiaoman.sid',
+      // 返回到前端的cookie key的属性
+      cookie: {
+        maxAge: 999999,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
