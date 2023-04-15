241ec958f21c06c90d2a563d4f6ac46131a31fa8
baizhi958216 <1475289190@qq.com>
2023-04-16 00:29:00 +0800

    feat: 生成demo模块
    通过 nestcli: nest g mo demo
    
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoController } from './demo/demo.controller';
 import { DemoService } from './demo/demo.service';
+import { DemoModule } from './demo/demo.module';
 
 @Module({
-  imports: [],
+  imports: [DemoModule],
   controllers: [AppController, DemoController],
   providers: [AppService, DemoService],
 })
