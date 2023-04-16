35e1bcf2b66418185abb1fce9889bb4452ecea7b
baizhi958216 <1475289190@qq.com>
2023-04-16 20:57:52 +0800

    feat: 上传文件
    formdata: http://localhost:3000/upload/album
    参数 file
    
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,10 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+  @Post('album')
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file) {
+    console.log(file, 'file');
+    return '小满憋不住了';
+  }
 }
