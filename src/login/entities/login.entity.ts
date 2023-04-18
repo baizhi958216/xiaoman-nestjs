784f6eb626f6608b2013cab36d178d9eb0d220a5
baizhi958216 <1475289190@qq.com>
2023-04-18 13:06:58 +0800

    feat: 连接数据库, 创建实体
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/login/entities/login.entity.ts
+++ src/login/entities/login.entity.ts
@@ -1,1 +1,14 @@
-export class Login {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Login {
+  // 自增id
+  @PrimaryColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  password: string;
+  @Column()
+  age: number;
+}
--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Login } from './entities/login.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Login])],
   controllers: [LoginController],
-  providers: [LoginService]
+  providers: [LoginService],
 })
 export class LoginModule {}
