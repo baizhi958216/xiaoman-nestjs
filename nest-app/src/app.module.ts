72f247e0e14ea57bc65128758406365cbfc4f6f2
baizhi958216 <1475289190@qq.com>
2023-04-19 01:07:22 +0800

    feat: nest连接数据库, 创建实体, 跨域处理
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: '123456',
+      host: 'localhost',
+      port: 3306,
+      database: 'firstcurd',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-app/src/user/entities/user.entity.ts
+++ nest-app/src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createTime: Date;
+
+  @Column({ type: 'text' })
+  desc: string;
+}
